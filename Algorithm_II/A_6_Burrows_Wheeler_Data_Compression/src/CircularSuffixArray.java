2e3f786c91ef27c0d2ac7761bd5ddec6c6418a70
jackalsin <jackal.sin@hotmail.com>
2016-06-29 23:12:37 -0400

    stat

---
+++ Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/CircularSuffixArray.java
@@ -0,0 +1,28 @@
+/**
+ * Given a text file in which sequences of the same character occur near each other many times, convert it into a text
+ * file in which certain characters appear more frequently than others.
+ *
+ * @author jacka
+ * @version 1.0 on 6/29/2016.
+ */
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // length of s
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
