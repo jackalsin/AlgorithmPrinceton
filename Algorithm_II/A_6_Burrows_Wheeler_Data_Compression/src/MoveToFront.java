beeb59b12abefd0d4a5e296455d498177fa7e2d1
jackalsin <jackal.sin@hotmail.com>
2016-06-29 23:44:31 -0400

    Implement encode

--- Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/MoveToFront.java
+++ Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/MoveToFront.java
@@ -1,11 +1,39 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 /**
  * @author jacka
  * @version 1.0 on 6/29/2016.
  */
 public class MoveToFront {
-    // apply move-to-front encoding, reading from standard input and writing to standard output
-    public static void encode() {
+    private static final int R = 256;
 
+    /**
+     * Initialize the sequence by making the ith character in the sequence equal to the ith
+     * extended ASCII character. Now, read in each 8-bit character c from standard input one
+     * at a time, output the 8-bit index in the sequence where c appears, and move c to the front.
+     */
+    public static void encode() {
+        char[] sequence = new char[R];
+        for (int i = 0; i < R; i++) {
+            sequence[i] = (char) i;
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+
+            for (int i = 0; i < R; i++) {
+                if (c == sequence[i]) {
+                    System.arraycopy(sequence, 0, sequence, 1, i - 1);
+                    sequence[0] = c;
+                    BinaryStdOut.write(i);
+                    break;
+                }
+            }
+
+        }
+        BinaryStdIn.close();
+        BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -16,6 +44,19 @@ public class MoveToFront {
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
     public static void main(String[] args) {
-
+        if (args == null)
+            throw new NullPointerException("args cannot be null");
+        if (args.length == 0)
+            throw new IllegalArgumentException("args cannot be an empty string");
+        switch (args[0]) {
+            case "-":
+                encode();
+                break;
+            case "+":
+                decode();
+                break;
+            default:
+                break;
+        }
     }
 }
