4345524a3e67658813345a0eda17a7b64d5fcd44
jackalsin <jackal.sin@hotmail.com>
2016-06-30 21:14:08 -0400

    backup decode

--- Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/BurrowsWheeler.java
+++ Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/BurrowsWheeler.java
@@ -1,23 +1,72 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 /**
  * This is implementing BurrowsWheeler Algorithm
- * 
+ *
  * @author jacka
  * @version 1.0 on 6/29/2016.
  */
 public class BurrowsWheeler {
     // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
     public static void encode() {
+        /* java BurrowsWheeler - < abra.txt */
+        /* A    B   R   A   C   A   D   A   B   R   A   !
+        *   */
+        String inputStr = BinaryStdIn.readString();
+        CircularSuffixArray csa = new CircularSuffixArray(inputStr);
+        // get First
+        int first = -1;
+        for (int i = 0; i < csa.length(); i++) {
+            if (csa.index(i) == 0) {
+                first = i;
+                BinaryStdOut.write(i);
+                break;
+            }
+        } // end of for loop
 
+        int csaLen = csa.length();
+        char[] result = new char[csaLen];
+
+        for (int i = 0; i < csaLen; i++) {
+            int index = csa.index(i);
+            if (csa.index(i) == 0)
+                result[i] = inputStr.charAt(csaLen - 1);
+            else {
+                result[i] = inputStr.charAt(index - 1);
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            BinaryStdOut.write(result[i], 8);
+        }
+        BinaryStdIn.close();
+        BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
     public static void decode() {
+        int first = BinaryStdIn.readInt();
+        String input = BinaryStdIn.readString();
+        char[] chars = input.toCharArray();
+        int[] next = new int[chars.length];
+        
+
 
     }
 
     // if args[0] is '-', apply Burrows-Wheeler encoding
     // if args[0] is '+', apply Burrows-Wheeler decoding
     public static void main(String[] args) {
+        if (args == null)
+            throw new NullPointerException("args cannot be null");
+        if (args.length == 0)
+            throw new IllegalArgumentException("args cannot be empty");
 
+        switch (args[0]) {
+            case "+": decode(); break;
+            case "-": encode(); break;
+            default:
+                throw new IllegalArgumentException("args[0] can only be '+' or '-'" );
+        }
     }
 }
---
+++ Algorithm_II/A_6_Burrows_Wheeler_Data_Compression/src/BurrowsWheelerTest.java
@@ -0,0 +1,64 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jacka
+ * @version 1.0 on 6/30/2016.
+ */
+public class BurrowsWheelerTest {
+
+    private static String DECODED_INPUT = "ABRACADABRA!";
+
+    private static byte[] ENCODED_INPUT = { 0x0, 0x0, 0x0, 0x3,
+            0x41, 0x52, 0x44, 0x21, 0x52, 0x43, 0x41, 0x41, 0x41,
+            0x41, 0x42, 0x42 };
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void encodeTest() throws Exception {
+        System.setIn(new ByteArrayInputStream(DECODED_INPUT.getBytes()));
+        // create new output stream as byte array and assign to standard
+        ByteArrayOutputStream resultOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(resultOutputStream));
+
+        BurrowsWheeler.encode();
+        byte[] encoded = resultOutputStream.toByteArray();
+
+        System.err.println("output = \t" + Arrays.toString(encoded));
+        System.err.println("reference = " + Arrays.toString(ENCODED_INPUT));
+        // 4 = 32/ 8 int is 4 bytes
+        for (int i = 0; i < 4; i++) {
+            assertEquals(ENCODED_INPUT[i], encoded[i]);
+        }
+        // letters
+        for (int i = 4; i < encoded.length; i++) {
+            assertEquals(ENCODED_INPUT[i], encoded[i]);
+        }
+
+        System.setIn(null);
+        System.setOut(null);
+    }
+
+    @Test
+    public void decode() throws Exception {
+
+    }
+    
+}
